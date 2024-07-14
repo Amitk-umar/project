e52f5a4b0b2a533f488f58418f50bbe5a4903b73
Amitk-umar <135205505+Amitk-umar@users.noreply.github.com>
2024-07-14 19:59:05 +0530

    Changes

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ document.addEventListener("DOMContentLoaded", function () {
         menu.classList.toggle('show');
     });
 });
+   
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cartCountElement = document.querySelector('.cart-count');
+    const addToCartButtons = document.querySelectorAll('.buy-button');
+    let cartCount = 0;
+  
+    addToCartButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        cartCount++;
+        cartCountElement.textContent = cartCount;
+        showModal();
+      });
+    });
+  
+    const modal = document.getElementById("cartModal");
+    const span = document.getElementsByClassName("close")[0];
+  
+    function showModal() {
+      modal.style.display = "block";
+    }
+  
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
+  
